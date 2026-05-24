559186de62380aca9cda6937bfada1eabf41712c
Guilherme Lima <guilhermefk123@gmail.com>
2026-05-24 13:58:17 -0300

    feat: configurar rotas do CRUD de usuarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,18 @@
-import express from 'express';
-import {createUser, getALLUsers, deleteUser} from ('./controllers/userController')
+import express from "express";
+import {
+  createUser,
+  getUsers,
+  getUserID,
+  editUser,
+  deleteUser,
+} from "../controllers/userController.js";
+
 const router = express.Router();
 
-router.post("/usuario", adicionarUser);
-router.get("/usuarios", listarUser);
-router.get("/usuario/:id", buscarUserId);
-router.put("/usuario/:id", editUser);
-router.delete("/usuario/:id", deleteUser);
+router.post("/", createUser);      
+router.get("/", getUsers);         
+router.get("/:id", getUserID);     
+router.put("/:id", editUser);     
+router.delete("/:id", deleteUser); 
 
 export default router;
