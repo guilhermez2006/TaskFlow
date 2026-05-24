d1a2892cc60850a46db690ca4c80ec64f76fbb86
Guilherme Lima <guilhermefk123@gmail.com>
2026-05-24 13:58:44 -0300

    refactor: ajustar dependencias, server e controllers para o CRUD

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,30 @@
 export const createUser = (req, res) => {
+
+
   res.status(200).json({ message: "Deu bom!" });
 };
 
-export const getALLUsers = (req, res) => {
+export const getUsers = (req, res) => {
+
+
   res.status(200).json({ message: "Deu bom!" });
 };
 
 export const getUserID = (req, res) => {
+
+
   res.status(200).json({ message: "Deu bom!" });
 };
 
 export const editUser = (req, res) => {
+
+
   res.status(200).json({ message: "Deu bom!" });
 };
 
 export const deleteUser = (req, res) => {
+
+  
   res.status(200).json({ message: "Deu bom!" });
 };
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import userRoutes from './routes.js'
+import userRoutes from './routes/routes.js'; 
 
 const app = express();
 app.use(express.json());
 
 app.use('/usuarios', userRoutes);
 
-app.listen(3000, () => console.log("Servidor ON"));
+app.listen(3000, () => console.log("🚀 Servidor ON na porta 3000"));
