265b680f49f674dc5e0bc0130bdf746bd3339897
Guilherme Lima <guilhermefk123@gmail.com>
2026-05-27 23:44:40 -0300

    efactor: limpar server.js, ajustar rotas e adicionar CORS

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-import express from 'express';
-import userRoutes from './routes/routes.js'; 
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/routes.js";
 
 const app = express();
-app.use(express.json());
 
-app.use('/usuarios', userRoutes);
+app.use(express.json());
+app.use(cors());
+
+app.use(userRoutes);
 
 app.listen(3000, () => console.log("🚀 Servidor ON na porta 3000"));
