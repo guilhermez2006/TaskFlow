e14c65d1c6fff3abdd269ba791afa2681188b5e5
Guilherme Lima <guilhermefk123@gmail.com>
2026-08-11 09:59:48 -0300

    feat(server): configure application entry point and server startup

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/routes.js";
+import "./database/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(userRoutes);
+// Define que todas as rotas do userRoutes terão o prefixo /users
+app.use("/users", userRoutes);
 
 app.listen(3000, () => console.log("🚀 Servidor ON na porta 3000"));
